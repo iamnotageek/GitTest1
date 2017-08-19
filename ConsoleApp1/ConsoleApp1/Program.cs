30330e90e99b5125450fdc9f6dd3a5ac97d60d3c
iamnotageek <mancanhold@gmail.com>
2017-08-19 14:42:13 +0000

    added Feature1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,9 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //Code was edited in Github
-            // code was added in VS
+            //Code was added in VS
+
+            //Code to call Feature 1
         }
     }
 }
